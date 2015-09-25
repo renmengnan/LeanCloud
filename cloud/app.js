85302a4874c0a2c8454b2449d9dd362ba62daf9d
renmengnan <ren_mn@126.com>
2015-09-25 18:05:17 +0800

    test time 2

--- cloud/app.js
+++ cloud/app.js
@@ -184,7 +184,7 @@ function transformSearchTicket(t) {
         restaurantReceiver: t.restaurantReceiver,
         restaurantTel: t.restaurantTel,
         orderId: t.orderId,
-        createdAt: moment(t.createdAt).format('YYYY-MM-DD HH:mm:ss'),
+        createdAt: moment(t.createdAt+'+0800').format('YYYY-MM-DD HH:mm:ss'),
         createdAtUnix: moment(t.createdAt).valueOf()
     };
 }
@@ -196,10 +196,10 @@ function formatTime(t) {
 }
 
 function formatTimeLong(t) {
-    var time = moment(t).tz('Asia/Shanghai');
+    // var time = moment(t).tz('Asia/Shanghai');
     // console.log('toString', time.format('YYYY-MM-DD HH:mm:ss'));
     // console.log('getHours', time.hours())
-    var date = time.format('YYYY-MM-DD HH:mm:ss');
+    var date = moment(t+'+0800').format('YYYY-MM-DD HH:mm:ss');
     return date;
 }
 
