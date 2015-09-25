1a175427cdedaa628ed1b1b0467a82a1865cdba2
renmengnan <ren_mn@126.com>
2015-09-25 18:42:20 +0800

    test time 2

--- cloud/app.js
+++ cloud/app.js
@@ -199,11 +199,12 @@ function formatTimeLong(t) {
     // var time = moment(t).tz('Asia/Shanghai');
     // console.log('toString', time.format('YYYY-MM-DD HH:mm:ss'));
     // console.log('getHours', time.hours())
-    var date = moment(t+'z').format('YYYY-MM-DD HH:mm:ss');
+    var date = moment(t).format('YYYY-MM-DD HH:mm:ss');
     return date;
 }
 
 function transformTicket(t) {
+    console.log(moment(t.createdAt).format('YYYY-MM-DD HH:mm:ss'))
     var rawStatus = t.get('status');
     var open = secret_content;
     if (t.get('open') == open_content) {
