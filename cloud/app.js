3ec3a92f622e8a0fc6262dd586cab6edaecc389d
renmengnan <ren_mn@126.com>
2015-10-26 17:52:40 +0800

    test

--- cloud/app.js
+++ cloud/app.js
@@ -209,6 +209,8 @@ function formatTimeLong(t) {
     // console.log('toString', time.format('YYYY-MM-DD HH:mm:ss'));
     // console.log('getHours', time.hours())
     var date = moment(t).format('YYYY-MM-DD HH:mm:ss');
+    console.log(data);
+    console.log(moment(t));
     return date;
 }
 
