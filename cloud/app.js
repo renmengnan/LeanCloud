5a48a5d4327136b3ce9c0766d90de98d2cd2eb99
renmengnan <ren_mn@126.com>
2015-10-27 11:57:36 +0800

    修复时间bug

--- cloud/app.js
+++ cloud/app.js
@@ -10,8 +10,7 @@ var app = express();
 var Mailgun = require('mailgun').Mailgun;
 var util = require('util');
 var expressLayouts = require('express-ejs-layouts');
-var moment = require('moment');
-var moments = require('moment-timezone');
+var moment = require('moment-timezone');
 var _ = require('underscore');
 var fs = require('fs');
 var avosExpressHttpsRedirect = require('avos-express-https-redirect');
@@ -206,8 +205,8 @@ function formatTime(t) {
 }
 
 function formatTimeLong(t) {
-    var time = moments(t).tz('Asia/Shanghai');
-    console.log('toString', time.format('YYYY-MM-DD HH:mm:ss'));
+    // var time = moment(t).tz('Asia/Shanghai');
+    // console.log('toString', time.format('YYYY-MM-DD HH:mm:ss'));
     // console.log('getHours', time.hours())
     var date = moment(t).format('YYYY-MM-DD HH:mm:ss');
     return date;
